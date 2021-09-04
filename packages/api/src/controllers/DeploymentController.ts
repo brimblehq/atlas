db793f489216174d012ca2f900e54f9167111868
klefcodes <abrahamugbeshe@gmail.com>
2021-09-04 11:47:56 +0100

    feat: worked on searching for user repos
    feat #5

--- packages/api/src/controllers/DeploymentController.ts
+++ packages/api/src/controllers/DeploymentController.ts
@@ -3,10 +3,35 @@ import { responseData } from "../helpers";
 
 export class DeploymentController {
   public async getRepos(req: Request, res: Response) {
+    const { q } = req.query as any;
     const me = await req.body.ghclient.me();
-    const data = await me.reposAsync();
-    return res.json(responseData("OK", false, 200, data));
+    let data = await me.reposAsync();
+
+    if (q) {
+      data = data[0]?.filter((repo: any) => {
+        return q
+          .toLowerCase()
+          .split(" ")
+          .every((v: string) => repo.name.toLowerCase().includes(v));
+      });
+    }
+
+    if (data.length) {
+      return res.json(responseData("OK", false, 200, data));
+    }
+
+    return res
+      .status(404)
+      .json(
+        responseData(
+          `Repository with this name "${q}" not found`,
+          true,
+          404,
+          data
+        )
+      );
   }
+
   public async selectRepo(req: Request, res: Response) {
     const repo = await req.body.ghclient.repo(req.body.repo);
     const data = await repo.contentsAsync("package.json", "main").catch(() => {
