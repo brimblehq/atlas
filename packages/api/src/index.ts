3ae7966c27667323bfdb2f8bb7ceeb6cd7cc373e
klefcodes <abrahamugbeshe@gmail.com>
2021-09-05 21:11:25 +0100

    perf: added logger

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,7 +1,8 @@
+import { log } from "@brimble/utils";
 import app from "../app";
 
 const PORT = process.env.PORT || 9999;
 
 app.listen(PORT, () => {
-  console.log(`Serving on http://localhost:${PORT} 🚀`);
+  log.info(`Serving on http://localhost:${PORT} 🚀`);
 });
--- packages/models/index.ts
+++ packages/models/index.ts
@@ -1,18 +1,18 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import { log } from "@brimble/utils";
 // Connection to Mongo
 (async (): Promise<void> => {
   dotenv.config();
 
   const mongoUrl: string = `${process.env.MONGODB_URI}`;
-  console.log(mongoUrl);
   const options = { useNewUrlParser: true, useUnifiedTopology: true };
   mongoose.set("useFindAndModify", false);
   try {
     await mongoose.connect(mongoUrl, options);
-    console.log(`Database connection successful`);
+    log.info(`Database connection successful`);
   } catch (error) {
-    console.log(`Error connecting to DB`, error);
+    log.error(`Error connecting to DB`, error);
     return process.exit(1);
   }
 })();
