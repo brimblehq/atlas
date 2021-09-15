f331e1a02ba348f7e459f43347bb50e2b3d0cc8e
klefcodes <abrahamugbeshe@gmail.com>
2021-09-15 07:46:14 +0100

    test: removed helper test

--- packages/api/src/helpers/__test__/responseData.test.ts
+++
@@ -1,6 +0,0 @@
-const { responseData } = require("../index");
-it("should return an object containing {status: 200}", () => {
-  expect(responseData("OK", false, 200)).toEqual(
-    expect.objectContaining({ status: 200 })
-  );
-});
