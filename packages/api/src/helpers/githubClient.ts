4d04fe556b7ee5a84bbaad2882bb4ea64dae4e00
klefcodes <abrahamugbeshe@gmail.com>
2021-12-05 17:13:06 +0100

    chore: setup github client

--- packages/api/src/helpers/githubClient.ts
+++ packages/api/src/helpers/githubClient.ts
@@ -1,7 +1,29 @@
-const github = require("octonode");
+import { createAppAuth } from "@octokit/auth-app";
+import axios from "axios";
 
-const githubClient = (token: string) => {
-  return github.client(token);
-};
+async function createJWT(installationId: number) {
+  const auth = createAppAuth({
+    appId: process.env.BRIMBLE_BUILD_ID || "",
+    privateKey: process.env.BRIMBLE_BUILD_PRIVATE_KEY || "",
+    installationId,
+    clientId: process.env.GITHUB_CLIENT_ID || "",
+    clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+  });
 
-export default githubClient;
+  const { token } = await auth({ type: "installation" });
+  return token;
+}
+
+async function githubRequest(url: string, installationId: number) {
+  const token = await createJWT(installationId);
+
+  const { data } = await axios.get(`https://api.github.com${url}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return data;
+}
+
+export default githubRequest;
