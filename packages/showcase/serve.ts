30f0d76a2fb9f4a88e040979ce2b933385a2a4b2
klefcodes <abrahamugbeshe@gmail.com>
2021-12-27 20:28:13 +0100

    wip: to be improved upon. Render nested html files

--- packages/showcase/serve.ts
+++ packages/showcase/serve.ts
@@ -6,8 +6,8 @@ import history from "connect-history-api-fallback";
 
 const serve = async (folder: string = ".") => {
   try {
-    const files = fs.readdirSync(folder);
-    const index = files.find((file) => file.endsWith("index.html"));
+    const files = fs.readdirSync(folder, { withFileTypes: true });
+    const index = files.find((file) => file.name.endsWith("index.html"));
     if (!index) {
       throw new Error("No index.html found");
     }
@@ -15,20 +15,35 @@ const serve = async (folder: string = ".") => {
     // Serve static file
     const app = express();
     const PORT = await getPort({ port: 3000 });
-    const htmlFiles = files.filter((file) => file.endsWith(".html"));
+    const htmlFiles = files.filter((file) => file.name.endsWith(".html"));
+    const directory = files.filter((de) => de.isDirectory());
+    const rewrites = [
+      ...directory.map((dir) => {
+        let dirFiles = fs.readdirSync(`${folder}/${dir.name}`, {
+          withFileTypes: true,
+        });
+        const mapDirs = dirFiles.map((file) => {
+          const name = file.name.replace(".html", "");
+          return {
+            from: new RegExp(`/${dir.name}/${name}`),
+            to: `/${dir.name}/${file.name}`,
+          };
+        });
+        return mapDirs;
+      }),
+      ...htmlFiles.map((file) => {
+        const name = file.name.replace(".html", "");
+        return {
+          from: new RegExp(`/${name}`),
+          to: `/${file.name}`,
+        };
+      }),
+    ]
+      .filter(Boolean)
+      .flat();
 
     app.use(express.static(folder));
-    app.use(
-      history({
-        rewrites: htmlFiles.map((file) => {
-          const name = file.replace(".html", "");
-          return {
-            from: new RegExp(`/${name}`),
-            to: `/${file}`,
-          };
-        }),
-      })
-    );
+    app.use(history({ rewrites }));
     app.use(express.static(folder));
     app.listen(PORT, () => {
       console.log(chalk.green(`Serving to 👉🏻 http://127.0.0.1:${PORT}`));
