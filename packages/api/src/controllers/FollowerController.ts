a733e3738191c3cd80e373ba7c4057e96dfe60fd
klefcodes <abrahamugbeshe@gmail.com>
2022-02-16 12:34:36 +0100

    style: enforced code styling

--- packages/api/src/controllers/FollowerController.ts
+++ packages/api/src/controllers/FollowerController.ts
@@ -4,19 +4,19 @@ import { defaultErrorDto } from "@/types";
 import { Request, Response } from "express";
 
 export class FollowerController {
-    async followUser(req: Request, res: Response): Promise<Response> {
-        try {
-            const { id } = req.params;
-            let save = await FollowerService.followUser(id);
-            
-            return res.status(201).json(responseData("OK", save));
-        } catch (e) {
-            const { message, statusCode } = e as defaultErrorDto;
-            return res.status(statusCode).json(responseData(message));
-        }
+  async followUser(req: Request, res: Response): Promise<Response> {
+    try {
+      const { id } = req.params;
+      const save = await FollowerService.followUser(id);
+
+      return res.status(201).json(responseData("OK", save));
+    } catch (e) {
+      const { message, statusCode } = e as defaultErrorDto;
+      return res.status(statusCode).json(responseData(message));
     }
+  }
 
-    async fetchFollowers(){}
+  async fetchFollowers() {}
 
-    async unFollowUser(){}
+  async unFollowUser() {}
 }
