f2349420c3f8d5e12d52cea21cfa6e298b760057
Abraham <abrahamugbeshe@gmail.com>
2022-06-08 08:35:13 +0100

    refactor: move coming soon to another repo

--- packages/client/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import VueToast from "vue-toast-notification";
-import "vue-toast-notification/dist/theme-sugar.css";
-import App from "./App.vue";
-import "./index.css";
-
-createApp(App).use(VueToast).mount("#app");
