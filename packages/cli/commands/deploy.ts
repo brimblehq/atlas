b322128058ba51eddaa6370ced49009e740542dc
Abraham <abrahamugbeshe@gmail.com>
2022-08-22 17:07:48 +0100

    fix: minor bug fix

--- packages/cli/commands/deploy.ts
+++ packages/cli/commands/deploy.ts
@@ -319,7 +319,6 @@ const sendToServer = async ({
     .then(() => {
       config.set(`${name}`, {
         projectID,
-        domain,
         name,
         buildCommand,
         outputDirectory,
