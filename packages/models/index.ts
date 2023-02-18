e37044ac57862569ec278a59f4daac982e702e48
Muritala David <davmuri1414@gmail.com>
2023-02-18 19:29:01 +0100

    perf: export db

--- packages/models/index.ts
+++ packages/models/index.ts
@@ -56,6 +56,9 @@ export const connectToMongo = async (mongoUrl: string): Promise<void> => {
   });
 };
 
+export const db = mongoose.connection;
+
 export const closeMongo = () => {
   mongoose.connection.close(true);
 };
+
