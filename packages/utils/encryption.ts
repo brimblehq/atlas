e7841b98aa2ffa7f1f30589a4db91cb0796ab5f7
Muritala David <davmuri1414@gmail.com>
2023-03-23 13:11:36 +0100

    base64 fix

--- packages/utils/encryption.ts
+++ packages/utils/encryption.ts
@@ -1,16 +1,17 @@
 import CryptoJS from "crypto-js";
 
 export const encrypt = (value: string, key: string) => {
+    
     return CryptoJS.AES.encrypt(value, key).toString();
 }
   
 export const decrypt = (ciphertext: string, key: string) => {
     const bytes  = CryptoJS.AES.decrypt(ciphertext, key);
     const base64string =  bytes.toString(CryptoJS.enc.Utf8);
-    const value = atob(base64string);
+    const value = Buffer.from(base64string, 'base64').toString('utf8');
     return value.split(":")[2];
 }
 
 export const generateEncryptionKey = (projectId: string, userId: string, key: string) => {
-    return btoa(`${projectId}:${userId}:${key}`)
+    return Buffer.from(`${projectId}:${userId}:${key}`, 'binary').toString('base64');
 }
