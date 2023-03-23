ba5d3f49d6a62f3e0c083f2f9a67741640049470
Muritala David <davmuri1414@gmail.com>
2023-03-23 10:43:57 +0100

    generate enc key

--- packages/utils/encryption.ts
+++ packages/utils/encryption.ts
@@ -6,7 +6,9 @@ export const encrypt = (value: string, key: string) => {
   
 export const decrypt = (ciphertext: string, key: string) => {
     const bytes  = CryptoJS.AES.decrypt(ciphertext, key);
-    return bytes.toString(CryptoJS.enc.Utf8);
+    const base64string =  bytes.toString(CryptoJS.enc.Utf8);
+    const value = atob(base64string);
+    return value.split(":")[2];
 }
 
 export const generateEncryptionKey = (projectId: string, userId: string) => {
