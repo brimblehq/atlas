eee22d3dff53332fb6a541bd9b4e7600aec0dd10
Muritala David <davmuri1414@gmail.com>
2023-04-28 08:54:39 +0100

    exporting frameworks

--- packages/utils/framework.ts
+++ packages/utils/framework.ts
@@ -1,4 +1,4 @@
-import { frameworks } from "./index";
+import { default as frameworks } from "./constants/frameworks.json";
 
 const detectFramework = (packageJson: any) => {
   const detectFramework = frameworks.find(
