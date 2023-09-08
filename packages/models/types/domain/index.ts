16e7ad1b6b72017f4285691f73887d9c26334959
Muritala David <davmuri1414@gmail.com>
2023-09-08 09:20:03 +0100

    republish

--- packages/models/types/domain/index.ts
+++ packages/models/types/domain/index.ts
@@ -4,28 +4,16 @@ import { IDns } from "./dns";
 
 export interface IDomain extends Document {
   name: string;
-
   project: IProject;
-
   user_id: string;
-
   team_id: string;
-
   primary: boolean;
-
   preview: IPreview;
-
   is_free: boolean;
-
   purchased: boolean;
-
   privacy_enabled: boolean;
-
   renewal_date: string;
-
   renewal_price: number;
-  
   nameservers: string[];
-  
   dns: IDns[];
 }
