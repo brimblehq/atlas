d2496199229bba3d9d4e4673c9dad2793ec9e908
Muritala David <davmuri1414@gmail.com>
2023-09-22 12:40:58 +0100

    random Name package

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,4 +1,5 @@
 export { log, createLogger } from "./logger";
 export { detectFramework, allFrameworks } from "./framework";
 export { encryptValue, decryptValue } from "./encryption";
+export { randomName } from "./name";
 export { default as frameworks } from "./constants/frameworks.json";
