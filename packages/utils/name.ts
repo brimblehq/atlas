a4189cc8b4e60cbab909c2c876e001c782667369
Muritala David <davmuri1414@gmail.com>
2023-09-22 12:38:42 +0100

    random Name package

---
+++ packages/utils/name.ts
@@ -0,0 +1,7 @@
+import { adjectives, animals, colors, uniqueNamesGenerator, Config } from 'unique-names-generator';
+
+export const randomName = (config?: Config) : string => uniqueNamesGenerator(config || {
+    dictionaries: [adjectives, colors, animals],
+    separator: '-',
+    length: 2,
+});
