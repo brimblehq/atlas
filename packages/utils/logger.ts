2a7c8acc9ca1f0bfb34cf2b891f8d7fd90772dec
Abraham <abrahamugbeshe@gmail.com>
2023-10-04 11:03:35 +0100

    perf(utils): added timestamp to logger

--- packages/utils/logger.ts
+++ packages/utils/logger.ts
@@ -2,7 +2,10 @@ import winston, { LoggerOptions } from "winston";
 
 const defaultOptions = {
   level: "info",
-  format: winston.format.json(),
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
   transports: [
     new winston.transports.Console({ format: winston.format.json() }),
   ],
