03a589195b66e086814cd6986150f0068444105e
Muritala David <davmuri1414@gmail.com>
2024-03-11 09:25:51 +0100

    updates

--- packages/models/permission.ts
+++ packages/models/permission.ts
@@ -5,7 +5,9 @@ import { PERMISSION_TYPE } from "./enum";
 const permissionSchema = new Schema({
     title: { type: String },
     role: [{ type: Schema.Types.ObjectId, ref: 'Role' }],
-    type: { type: String, enum: Object.values(PERMISSION_TYPE) }
+    request: { type: Object },
+    type: { type: String, enum: Object.values(PERMISSION_TYPE) },
+    enabled: { type: Boolean, default: false, select: true }
 });
 
 export default model<IPermission>("Permission", permissionSchema);
