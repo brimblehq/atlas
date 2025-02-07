68d9dec00605fa93d0ed74ec4a14ab63da9a2b3d
Muritala David <davmuri1414@gmail.com>
2025-02-07 07:52:58 +0100

    plan configurations

---
+++ packages/models/plan_configuration.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { SUBSCRIPTION_PLAN_TYPE } from './enum';
+import { IPlanConfiguration } from './types/plan_configuration';
+
+const PlanConfigurationSchema = new Schema<IPlanConfiguration>({
+  tag: { type: String, required: true },
+  slack_support: { type: Boolean, default: false },
+  concurrent_builds: { type: Number, required: true },
+  project_limit: { type: Number, required: true },
+  deploy_private_organization: { type: Boolean, default: false },
+  preview_comments: { type: Boolean, default: false },
+  analytics: { type: Boolean, default: false },
+  build_minutes: { type: Number, required: true },
+  log_retention: { type: Number, required: true },
+  bandwidth: { type: Number, required: true },
+  plan_type: { type: String, enum: Object.values(SUBSCRIPTION_PLAN_TYPE), required: true },
+  memory: { type: Number, required: true },
+  build_timeout: { type: Number, required: true },
+  storage: { type: Number, required: true },
+  cpu: { type: Number, required: true },
+}, { timestamps: true });
+
+const PlanConfiguration = mongoose.model<IPlanConfiguration>('PlanConfigurations', PlanConfigurationSchema);
+
+export default PlanConfiguration;
---
+++ packages/models/types/plan_configuration.ts
@@ -0,0 +1,21 @@
+import { Document } from "mongoose";
+import { SUBSCRIPTION_PLAN_TYPE } from "../enum";
+
+export interface IPlanConfiguration extends Document {
+    tag: string;
+    slack_support: boolean;
+    concurrent_builds: number;
+    project_limit: number;
+    deploy_private_organization: boolean;
+    preview_comments: boolean;
+    analytics: boolean;
+    build_minutes: number;
+    log_retention: number;
+    bandwidth: number;
+    plan_type: SUBSCRIPTION_PLAN_TYPE;
+    memory: number;
+    build_timeout: number;
+    storage: number;
+    cpu: number;
+  }
+  
